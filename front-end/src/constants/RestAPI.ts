f10150db2cdb66222c872dff712be123c35521b5
drwolf1999 <kdy19991222@gmail.com>
2021-06-26 18:39:12 +0900

    add .env file

--- front-end/src/constants/RestAPI.ts
+++ front-end/src/constants/RestAPI.ts
@@ -1,4 +1,4 @@
-const SERVER_DOMAIN = 'http://172.30.1.7:3000';
+const SERVER_DOMAIN = `http://${process.env.VUE_APP_REST_API_URL}:${process.env.VUE_APP_REST_API_PORT}`;
 
 export default {
     SERVER_DOMAIN: SERVER_DOMAIN,
